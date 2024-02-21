c5fb6603dc739ccdf5e12f04b44cab1a9c6233db
Githuba Jeremy <githuba9520@gmail.com>
2024-02-21 12:15:26 +0300

    initial commit

---
+++ Hello.py
@@ -0,0 +1,25 @@
+print("Hello World!")
+age = str(20)
+print (age)
+
+isOnline = True
+
+
+name = "John"
+
+isNewPatient = True
+
+print(name + " is " +  age + " years old")
+
+dec = 20.22
+print(type(dec))
+
+
+# doctor = input("Kindly enter your name: ")
+# print("Welcome " + doctor)
+
+
+
+birth_year = input("What year were you born? ")
+years = 2024 - int(birth_year)
+print("You are "  + str(years) + " years old")
---
+++ calc.py
@@ -0,0 +1,7 @@
+print("Enter the two numbers you want to add: ")
+first_number = float(input("First Number: "))
+second_number = float(input("second Number: "))
+
+result = (first_number) + (second_number)
+
+print(result)
